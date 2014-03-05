5692aee853f4e0e72947898cd4f4a16987f699c0
kuronekomichael <michael@cat.email.ne.jp>
2014-03-05 21:18:17 +0900

    add GenyMotion

--- modules/people/manifests/kuronekomichael.pp
+++ modules/people/manifests/kuronekomichael.pp
@@ -136,6 +136,9 @@ class people::kuronekomichael {
       'LibreOffice':
         source => "http://download.documentfoundation.org/libreoffice/stable/4.2.1/mac/x86/LibreOffice_4.2.1_MacOS_x86.dmg",
         provider => appdmg;
+      'GenyMotion':
+        source => "http://files2.genymotion.com/genymotion/genymotion-2.1.1/genymotion-2.1.1.dmg",
+        provider => appdmg;
 # manual install
 #      'cooViewer':
 #        source => "http://www.geocities.jp/coo_ona/cooviewer1.2b24.dmg",
